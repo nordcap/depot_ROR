e82fa0eaf0a44ab7f21a176499ad16e3b6382b3e
Alex Budaev <budaevaa@mail.ru>
2016-06-25 06:20:16 +0700

    Create styles for Product:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.delete_all
+Product.create!(
+           title: 'Ноутбук DELL',
+           description: "<p> Замечательный рабочий ноутбук который вам послужит хорошую службу </p>",
+           image_url: 'notebook.png',
+           price: 30000
+)
+
+Product.create!(
+    title: 'Монитор SAMSUNG',
+    description: "<p>Самый лучший монитор с большим сроком гарантии</p>",
+    image_url: 'monitor.png',
+    price: 10000
+)
+Product.create!(
+    title: 'Роутер D-LINK',
+    description: "<p>Роутер с скоростью 100 мб/с</p>",
+    image_url: 'router.png',
+    price: 2000
+)
